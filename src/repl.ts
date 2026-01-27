ff7c4b7c73b3f51a01c114f4cc7bf7f670196130
Adam <145332225+useradamlee@users.noreply.github.com>
2026-01-27 09:45:10 +0800

    Add rudimentary REPL functionality

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,6 @@
+import { exit } from "process";
+import readline from "readline";
+
 export function cleanInput(input: string): string[] {
   const split_text = input.split(" ");
   let cleaned_text: string[] = [];
@@ -7,4 +10,22 @@ export function cleanInput(input: string): string[] {
     }
   }
   return cleaned_text;
+}
+
+export function startREPL() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex > ",
+  });
+
+  rl.prompt();
+  rl.on("line", (input) => {
+    const cleaned_input = cleanInput(input);
+    if (!input) {
+      rl.prompt();
+      exit();
+    }
+    console.log(`Your command was: ${cleaned_input[0]}`);
+  });
 }
